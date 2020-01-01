4a1bb8427a44389b5fd0bffb73b026ea814686d4
Marty Kane <marty.kane@gmail.com>
2020-01-01 12:21:35 -0600

    init canvas and drawing context, add basic redraw step and grid drawing

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
-let drawGrid = true;
+// Game Constants
+const gridSize = 50;
+const gridThickness = 2;
+const gridColor = 'rgb(30, 30, 30)';
+
+// UI State
+let showGrid = true;
+
+// Graphics Context from view
+let g; //graphics context
+let screenWidth = 300;
+let screenHeight = 150;
+
+function drawBackground() {
+    g.save();
+
+    g.fillStyle = '#eee';
+    g.fillRect(0, 0, screenWidth, screenHeight);
+
+    g.restore();
+}
+
+function drawGrid() {
+    if (!showGrid)
+        return;
+
+    g.save();
+
+    g.strokeStyle = gridColor;
+    g.lineWidth = gridThickness;
+
+    for (let x = 1; x < screenWidth; x += gridSize) {
+        g.moveTo(x, 0);
+        g.lineTo(x, screenHeight);
+        g.stroke();
+    }
+
+    for (let y = 1; y < screenHeight; y += gridSize) {
+        g.moveTo(0, screenHeight - y);
+        g.lineTo(screenWidth, screenHeight - y);
+        g.stroke();
+    }
+
+    g.restore();
+}
+
+function init(canvasObj) {
+    g = canvasObj.getContext('2d');
+
+    screenWidth = canvasObj.width;
+    screenHeight = canvasObj.height;
+
+    console.log('init', canvasObj);
+    console.log('context', g);
+    console.log(`dimensions ${screenWidth} x ${screenHeight}`);
+
+    reset();
+}
 
 function redraw() {
-    console.log('redraw')
+    if (!g) {
+        console.error("Canvas not initialized");
+        return;
+    }
+    console.log('redraw');
+
+    drawBackground();
+    drawGrid();
 }
 
 function reset() {
@@ -13,11 +77,13 @@ function step() {
 }
 
 function toggleGrid() {
-    drawGrid = !drawGrid;
-    console.log(`grid ${drawGrid ? 'on' : 'off'}`)
+    showGrid = !showGrid;
+    console.log(`grid ${showGrid ? 'on' : 'off'}`);
+    redraw();
 }
 
 export default {
+    init,
     toggleGrid,
     redraw,
     reset,
